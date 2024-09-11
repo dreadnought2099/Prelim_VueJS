08c3aabe005e298ee5b3b0a236d2452781893ee9
dreadnought2099 <raymart.magallanes@bisu.edu.ph>
2024-09-11 15:19:56 +0800

    Initial commit

--- src/components/ItemList.vue
+++ src/components/ItemList.vue
@@ -9,25 +9,21 @@ const handleAddToCart = (productName) => {
     <h2>Product List</h2>
     <div class="product">
       <h3>Product 1</h3>
+      <img src="../assets/images/18UMGIM26291.rgb.jpg" alt="Product Image" />
       <p>Price: $100</p>
       <button @click="handleAddToCart('Product 1')">Add to cart</button>
     </div>
     <div class="product">
       <h3>Product 2</h3>
+      <img src="../assets/images/18UMGIM26291.rgb.jpg" alt="Product Image" />
       <p>Price: $69</p>
       <button @click="handleAddToCart('Product 2')">Add to cart</button>
     </div>
     <div class="product">
       <h3>Product 3</h3>
-
+      <img src="../assets/images/18UMGIM26291.rgb.jpg" alt="Product Image" />
       <p>Price: $18</p>
       <button @click="handleAddToCart('Product 3')">Add to cart</button>
     </div>
   </div>
 </template>
-
-<style>
-.product {
-  margin-bottom: 30px;
-}
-</style>
